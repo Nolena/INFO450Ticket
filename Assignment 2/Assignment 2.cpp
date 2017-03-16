1c7f3a01411216baabbac2b5ef763c6d50078c8d
Andrew Nolen <Andrew Nolen@DESKTOP-OD2BSHJ>
2017-03-16 00:01:39 -0400

    Update 12:00am 3/15/17

--- Assignment 2/Assignment 2.cpp
+++ Assignment 2/Assignment 2.cpp
@@ -4,87 +4,101 @@
 #include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
+#include <string>
 using namespace std;
 
-
-
-
-
-
-int main()
-{
-
-	}
 
 class Ticket
 {
 private:
 	int iD;
 	char status;
-	char type;
-	int priority;
-	string name[100];
-	string description[1000];
-	int userImpact;
+	string type;
+	char priority;
+	string name;
+	string description;
+	int userImpact = 0;
+	static int idGenerator;
 
 public:
-
-	Ticket(int i, char s, char t, int p, char n, char d, int u)
+	Ticket() {};
+	Ticket(static int idGenerator, int iD);
+	Ticket(int i, char s, string t, char p, string n, string d, int u)
 	{
 		iD = i, status = s, type = t, priority = p, name = n, description = d, userImpact = u;
-
 	}
-	void CaptureTicket()
-	{
-		char ticketQuestion;
-
-		cout << "Open new ticket? (Y)es or (N)o?" << endl;
-		cin >> ticketQuestion;
-
-		switch (ticketQuestion)
-		{
-		default:
-			cout << "Invalid response! Please try again." << endl;
-			break;
-
-		case 'Y':
-		case 'y':
-			cout << "What is the name of the caller?" << endl;
-			gets_s(name);
-			cout << "What is the issue type? (S)erver, (A)pplication, a(C)cess" << endl;
-			switch (type)
-			{
-			default:
-				cout << "Invalid response! Please try again." << endl;
-				break;
-
-			case 'S':
-			case 's':
-				type = 's';
-				break;
-
-			case 'A':
-			case 'a':
-				type = 'a';
-				break;
-
-			case 'C':
-			case 'c':
-				type = 'c';
-				break;
-			}
-			cout << "Description of issue?" << endl;
-			getline (description);
-			cout << "How many users are impacted?" << endl;
-			cin >> userImpact
-			break;
-		case 'N':
-		case 'n':
-
-			break;
-
-		}
-		void ShowTicket();
-		void CloseTicket();
-	}
+	void ShowTicket();
+	void CaptureTicket();
+	void CloseTicket();
 };
+
+Ticket::Ticket( static int idGenerator, int iD) 
+{
+	idGenerator = 1;
+	iD = idGenerator++;
+
+}
+	void Ticket::CaptureTicket()
+	{
+		int typeID;
+		cout << "What is the name of the caller?" << endl;
+		getline(cin, name);
+		cout << "What is the issue type? 0 = Server, 1 = Application, 2 = Access" << endl;
+		cin >> typeID;
+		if (typeID = 0)
+			type = "Server";
+		if (typeID = 1)
+			type = "Application";
+		if (typeID = 2)
+			type = "Access";
+		cout << "Description of issue?" << endl;
+		getline(cin, description);
+		cout << "How many users are impacted?" << endl;
+		cin >> userImpact;
+		if (userImpact < 10)
+			priority = 'LOW';
+		if (userImpact >= 10 && userImpact < 50)
+			priority = 'MED';
+		if (userImpact > 50)
+			priority = 'HIGH';
+	}
+
+	void Ticket::ShowTicket() 
+	{
+		cout << "----------------------" << endl;
+		cout << "Ticket ID: " << iD << endl;
+		cout << "Name: " << name << endl;
+		cout << "Type of issue: " << type << endl;
+		cout << "Description of issue: " << description << endl;
+		cout << "Status: " << status << endl;
+		cout << "Priority: " << priority << endl;
+		cout << "----------------------" << endl;
+	}
+
+	void Ticket::CloseTicket() 
+	{
+
+	}
+
+
+
+	int main()
+	{
+		const int MAX = 100;
+
+		Ticket myTickets[MAX];
+
+		for (int i = 0; i < MAX; i++)
+		{
+			myTickets[i].CaptureTicket();
+		}
+
+		for (int i = 0; i < MAX; i++)
+		{
+			myTickets[i].ShowTicket();
+		}
+
+		return 0;
+		
+	}
+
